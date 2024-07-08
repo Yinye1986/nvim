7024995d071d358732164542c53197b18db4c243
Chris Hung <yinye1986@gmail.com>
2024-07-08 13:57:30 +0800

    bak

--- lua/plugin/plugins/flit.lua
+++ lua/plugin/plugins/flit.lua
@@ -4,13 +4,18 @@ return {
         "ggandor/leap.nvim",
     },
     enabled = true,
-    keys = function()
-        ---@type LazyKeys[]
-        local ret = {}
-        for _, key in ipairs({ "f", "F", "t", "T" }) do
-            ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
-        end
-        return ret
+    config = function()
+        require('flit').setup {
+            keys = { f = 'f', F = 'F', t = 't', T = 'T' },
+            -- A string like "nv", "nvo", "o", etc.
+            labeled_modes = "v",
+            -- Repeat with the trigger key itself.
+            clever_repeat = true,
+            multiline = true,
+            -- Like `leap`s similar argument (call-specific overrides).
+            -- E.g.: opts = { equivalence_classes = {} }
+            opts = {}
+        }
     end,
-    opts = { labeled_modes = "nx" },
+    opt = {},
 }
